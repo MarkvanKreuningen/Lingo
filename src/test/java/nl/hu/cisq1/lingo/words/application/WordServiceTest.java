e55a46cdac6f96c85d8af5b11d317cefc47ac655
markvk14 <markvankreuningen@gmail.com>
2021-12-27 16:27:49 +0100

    changed maven.yaml

--- src/test/java/nl/hu/cisq1/lingo/words/application/WordServiceTest.java
+++ src/test/java/nl/hu/cisq1/lingo/words/application/WordServiceTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.context.annotation.Profile;
 
 import java.util.Optional;
 import java.util.stream.Stream;
@@ -24,6 +25,7 @@ import static org.mockito.Mockito.*;
  * - its methods are called by the test framework instead of a controller
  * - the WordService calls a test double instead of an actual repository
  */
+@Profile("ci")
 class WordServiceTest {
 
     @ParameterizedTest
