272218ef7d87ede77b4ff82097ea1f4fb0317466
markvk14 <markvankreuningen@gmail.com>
2022-01-23 19:22:55 +0100

    added game without tests

--- src/main/java/nl/hu/cisq1/lingo/trainer/domain/Game.java
+++ src/main/java/nl/hu/cisq1/lingo/trainer/domain/Game.java
@@ -1,5 +1,6 @@
 package nl.hu.cisq1.lingo.trainer.domain;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Game {
@@ -8,29 +9,46 @@ public class Game {
     private List<Round> rounds;
     private Progress progress;
 
+    public Game(Integer score, GameStatus status, List<Round> rounds, Progress progress) {
+        this.score = score;
+        this.status = status;
+        this.rounds = rounds;
+        this.progress = progress;
+    }
 
     public void startNewRound(String wordToGuess) {
-
+        new Round(wordToGuess, null, null);
+        this.progress.setRoundNumber(this.progress.getRoundNumber() + 1);
     }
 
     public void guess(String word) {
-
+        Round round = this.rounds.get(this.rounds.size() - 1);
+        round.guess(word);
     }
 
     public Progress showProgress() {
-        return null;
+        return this.progress;
     }
 
     public boolean isPlayerEliminated() {
-        return false;
+        return this.status == GameStatus.ELIMINATED;
     }
 
     public boolean isPlaying() {
-        return false;
+        return this.status == GameStatus.PLAYING;
     }
 
     public Integer provideNextWordLength() {
-        return 1;
+
+        Round round = this.rounds.get(this.rounds.size() - 1);
+        switch (round.getCurrentWordLength()) {
+            case 5:
+                return 6;
+            case 6:
+                return 7;
+            default:
+                return 5;
+        }
     }
 
 }
--- src/main/java/nl/hu/cisq1/lingo/trainer/domain/Progress.java
+++ src/main/java/nl/hu/cisq1/lingo/trainer/domain/Progress.java
@@ -4,6 +4,36 @@ import java.util.List;
 
 public class Progress {
     private Integer score;
-    private List<Character> hints;
+    private List<String> hints;
     private Integer roundNumber;
+
+    public Progress(Integer score, List<String> hints, Integer roundNumber) {
+        this.score = score;
+        this.hints = hints;
+        this.roundNumber = roundNumber;
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+
+    public List<String> getHints() {
+        return hints;
+    }
+
+    public void setHints(List<String> hints) {
+        this.hints = hints;
+    }
+
+    public Integer getRoundNumber() {
+        return roundNumber;
+    }
+
+    public void setRoundNumber(Integer roundNumber) {
+        this.roundNumber = roundNumber;
+    }
 }
